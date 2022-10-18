49210e728a3c452743bf88ea6d1646a827ac628c
InYoung Park <dlsdudg15@naver.com>
2022-10-18 13:39:03 +0900

    Update cluster.service.ts

--- src/services/cluster.service.ts
+++ src/services/cluster.service.ts
@@ -9,7 +9,7 @@ const numCPUs = os.cpus().length;
 @Injectable()
 export class ClusterService {
   static init(callback: Function): void {
-    if (cluster.isPrimary) {
+    if (cluster.isMaster) {
       U.logger.log(`Master server started on ${process.pid}`);
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
