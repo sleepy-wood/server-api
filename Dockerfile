735275608a9c8f034194c32fa74c40c95ad32437
Jaeuk Yi <24962972+jvvook@users.noreply.github.com>
2022-10-25 00:26:40 +0900

    chore: wip container logging

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM node:16
 
 WORKDIR /app
 
-COPY . .
+COPY package.json yarn.lock .
 RUN yarn install --frozen-lockfile
+COPY . .
+
+EXPOSE 3000
 
 CMD ["yarn", "start:container"]
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,12 @@
+import { path } from 'app-root-path';
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import { join } from 'path';
 import * as winston from 'winston';
 import 'winston-daily-rotate-file';
-import { path } from 'app-root-path';
-import { WinstonModule, utilities as nestWinstonModuleUtilities } from 'nest-winston';
-import { join } from 'path';
 
 const { combine, timestamp, printf } = winston.format;
 const isTest = process.env.NODE_ENV === 'test';
+const isContainer = process.env.NODE_ENV === 'container';
 const logDir = join(path, 'logs');
 const logFormat = printf(({ _timestamp, level, message }) => `${_timestamp} ${level}: ${message}`);
 const timezone = () => {
@@ -14,52 +15,58 @@ const timezone = () => {
   });
 };
 
-export const logger = WinstonModule.createLogger({
-  format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.splat(),
-        winston.format.colorize(),
-        winston.format.simple(),
-        winston.format.timestamp({ format: timezone }),
-        nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
-      ),
-      silent: isTest,
-    }),
-    new winston.transports.DailyRotateFile({
+export const logger = isContainer
+  ? WinstonModule.createLogger({
       level: 'info',
-      datePattern: 'YYYY-MM-DD',
-      dirname: `${logDir}/info`,
-      filename: `%DATE%.log`,
-      maxFiles: 30,
-      json: false,
-      zippedArchive: true,
-      format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
-      silent: isTest,
-    }),
-    new winston.transports.DailyRotateFile({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: `${logDir}/error`,
-      filename: `%DATE%.error.log`,
-      maxFiles: 30,
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
-      format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
-      silent: isTest,
-    }),
-    new winston.transports.DailyRotateFile({
-      level: 'warn',
-      datePattern: 'YYYY-MM-DD',
-      dirname: `${logDir}/warn`,
-      filename: `%DATE%.log`,
-      maxFiles: 30,
-      json: false,
-      zippedArchive: true,
-      format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
-      silent: isTest,
-    }),
-  ],
-});
+      format: winston.format.simple(),
+      transports: [new winston.transports.Console()],
+    })
+  : WinstonModule.createLogger({
+      format: combine(timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.splat(),
+            winston.format.colorize(),
+            winston.format.simple(),
+            winston.format.timestamp({ format: timezone }),
+            nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
+          ),
+          silent: isTest,
+        }),
+        new winston.transports.DailyRotateFile({
+          level: 'info',
+          datePattern: 'YYYY-MM-DD',
+          dirname: `${logDir}/info`,
+          filename: `%DATE%.log`,
+          maxFiles: 30,
+          json: false,
+          zippedArchive: true,
+          format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
+          silent: isTest,
+        }),
+        new winston.transports.DailyRotateFile({
+          level: 'error',
+          datePattern: 'YYYY-MM-DD',
+          dirname: `${logDir}/error`,
+          filename: `%DATE%.error.log`,
+          maxFiles: 30,
+          handleExceptions: true,
+          json: false,
+          zippedArchive: true,
+          format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
+          silent: isTest,
+        }),
+        new winston.transports.DailyRotateFile({
+          level: 'warn',
+          datePattern: 'YYYY-MM-DD',
+          dirname: `${logDir}/warn`,
+          filename: `%DATE%.log`,
+          maxFiles: 30,
+          json: false,
+          zippedArchive: true,
+          format: nestWinstonModuleUtilities.format.nestLike('sleepy-wood'),
+          silent: isTest,
+        }),
+      ],
+    });
