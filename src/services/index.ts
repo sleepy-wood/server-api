8ef6f6af67a2919a024cac0eb8ef3713d2b1499b
PIYoung <49637184+PIYoung@users.noreply.github.com>
2022-10-27 20:57:03 +0900

    setup services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,17 @@
 //@index('./*.ts', f => f.path !== `./index` ? `export * from '${f.path}'` : "")
 export * from './attachFile.service';
+export * from './bridge.service';
+export * from './bridgeLand.service';
 export * from './cluster.service';
 export * from './file.service';
+export * from './item.service';
+export * from './itemType.service';
 export * from './jwt.service';
+export * from './land.service';
+export * from './landDecoration.service';
+export * from './sleep.service';
+export * from './tree.service';
+export * from './treeDecoration.service';
+export * from './treeFlatFrequency.service';
+export * from './treeMinMax.service';
 export * from './user.service';
---
+++ src/services/tree.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import * as D from '../dtos';
+import * as E from '../entities';
+import * as I from '../interfaces';
+import * as U from '../utils';
+import { HttpException } from '../exceptions';
+
+const exclude = ['deletedAt'];
+
+@Injectable()
+export class TreeService {
+  constructor(
+    @InjectRepository(E.Tree)
+    private readonly tree: Repository<E.Tree>,
+  ) {}
+
+  async create(context: I.RequestWithUser, contextType: I.ContextType): Promise<any> {
+    // this is intentional
+  }
+
+  async findAll(context: I.RequestWithUser, contextType: I.ContextType): Promise<any> {
+    // this is intentional
+  }
+
+  async findOne(context: I.RequestWithUser, contextType: I.ContextType, id: number): Promise<any> {
+    // this is intentional
+  }
+
+  async update(context: I.RequestWithUser, contextType: I.ContextType, id: number): Promise<any> {
+    // this is intentional
+  }
+
+  async remove(context: I.RequestWithUser, contextType: I.ContextType, id: number): Promise<any> {
+    // this is intentional
+  }
+}
