9818637060dae82870e7109fe0dabf8e4dcdb039
PIYoung <49637184+PIYoung@users.noreply.github.com>
2022-10-27 18:36:28 +0900

    define bridge entity

--- src/entities/app/bridge.entity.ts
+++ src/entities/app/bridge.entity.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  Column,
   Entity,
   CreateDateColumn,
   UpdateDateColumn,
@@ -15,6 +16,55 @@ export class Bridge {
   @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: '다리 이름',
+  })
+  name: string;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'x 좌표',
+  })
+  positionX: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'y 좌표',
+  })
+  positionY: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'z 좌표',
+  })
+  positionZ: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'x 회전',
+  })
+  rotationX: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'y 회전',
+  })
+  rotationY: number;
+
+  @ApiProperty()
+  @Column({
+    nullable: false,
+    comment: 'z 회전',
+  })
+  rotationZ: number;
 
   @ApiProperty()
   @CreateDateColumn({ type: 'timestamp' })
