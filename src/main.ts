2bdfe7095a6258408a98a10aa94789cbd9c7e3cb
PIYoung <dlsdudg15@naver.com>
2022-12-09 21:04:07 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -62,19 +62,20 @@ async function bootstrap() {
     }),
   );
 
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: [`'self'`],
-          imgSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', 'blob:', 'data:', 'validator.swagger.io'],
-          styleSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', `'unsafe-inline'`],
-          fontSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net'],
-          scriptSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', `https: 'unsafe-inline'`, `'unsafe-eval'`],
+  !isDev &&
+    app.use(
+      helmet({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: [`'self'`],
+            imgSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', 'blob:', 'data:', 'validator.swagger.io'],
+            styleSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', `'unsafe-inline'`],
+            fontSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net'],
+            scriptSrc: [`'self'`, '*.googleapis.com', '*.jsdelivr.net', `https: 'unsafe-inline'`, `'unsafe-eval'`],
+          },
         },
-      },
-    }),
-  );
+      }),
+    );
   app.use(compression());
   app.use(cookieParser());
 
@@ -114,27 +115,32 @@ function generateSwagger(app: NestExpressApplication, localIp: string, port: num
     .setTitle('메타버스 슬리피우드 어플리케이션')
     .setDescription(
       '<h4>슬리피우드 REST APIs</h4><h5>nestJS</h5><h5>written by PIYoung</h5>' +
-      '<div id="token-table-div">' +
-      '<div>' +
-      '<h4>사용자 토큰 선택 <span>펼치기/접기</span></h4>' +
-      '<table border="1px solid black">' +
-      '<tr><th>아이디</th><th>토큰</th></tr>' +
-      '<tr><td>고현서</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY3OTU2MDMxLCJleHAiOjMzMjI1NTU2MDMxfQ.t9odr3sxIAGNI75l6NFzg4Pm3YKyll0sECYQ_WoV4vg</a></td></tr>' +
-      '<tr><td>강수현</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjY3OTU2MDUxLCJleHAiOjMzMjI1NTU2MDUxfQ.53nskwqJO7etEsQ0GJKEWDJzCfbkhI0CRtZ8hE4JNwQ</a></td></tr>' +
-      '<tr><td>조한나</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjY3OTU2MDYzLCJleHAiOjMzMjI1NTU2MDYzfQ.f3EHkluagJCPz5Pi5a7fkBGWnopWTkWZwFOUTo_L_kg</a></td></tr>' +
-      '<tr><td>박대렬</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjY3OTU2MDc3LCJleHAiOjMzMjI1NTU2MDc3fQ.bvIBhieQ4p0cjRz9Yv0Y-fgfOst9bGvSP9ozzkaUAjA</a></td></tr>' +
-      '<tr><td>박인영</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjY3OTU2MDkwLCJleHAiOjMzMjI1NTU2MDkwfQ.cUpFp3VOQ7uWFDPRa3VHkmkCuQRpYpGOxdEgUT4jk_Q</a></td></tr>' +
-      '<tr><td>이재욱</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjY3OTU2MTAzLCJleHAiOjMzMjI1NTU2MTAzfQ.JFn1BNt_4uSC-Z63W2xVHiukt7ZYkeXJ1FLg0llmfjQ</a></td></tr>' +
-      '<tr><td>현지현_6</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjY3OTU2MTM4LCJleHAiOjMzMjI1NTU2MTM4fQ.6sqMKewK_jQOSorNfNZR8qQUHIBb_dat3P4v-6inMgE</a></td></tr>' +
-      '<tr><td>현지현_7</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjY3OTU2MTYxLCJleHAiOjMzMjI1NTU2MTYxfQ.GIU54JYVdoTgcMhD9cG98W387MlXe142H3KO7mTBpk8</a></td></tr>' +
-      '<tr><td>현지현_8</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjY3OTU2MTcyLCJleHAiOjMzMjI1NTU2MTcyfQ.EijITOOKYNREmumrS-vwVKxEsLTnvlQWc0q4VsXAcz8</a></td></tr>' +
-      '<tr><td>현지현_9</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTY2Nzk1NjE4MSwiZXhwIjozMzIyNTU1NjE4MX0.FMOJKJZSsqtxjrVfnFZSseLyBcLA3MlcYl9RU7tUCMA</a></td></tr>' +
-      '</table>' +
-      '</div>' +
-      '</div>',
+        '<div id="token-table-div">' +
+        '<div>' +
+        '<h4>사용자 토큰 선택 <span>펼치기/접기</span></h4>' +
+        '<table border="1px solid black">' +
+        '<tr><th>아이디</th><th>토큰</th></tr>' +
+        '<tr><td>고현서</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY3OTU2MDMxLCJleHAiOjMzMjI1NTU2MDMxfQ.t9odr3sxIAGNI75l6NFzg4Pm3YKyll0sECYQ_WoV4vg</a></td></tr>' +
+        '<tr><td>강수현</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjY3OTU2MDUxLCJleHAiOjMzMjI1NTU2MDUxfQ.53nskwqJO7etEsQ0GJKEWDJzCfbkhI0CRtZ8hE4JNwQ</a></td></tr>' +
+        '<tr><td>조한나</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjY3OTU2MDYzLCJleHAiOjMzMjI1NTU2MDYzfQ.f3EHkluagJCPz5Pi5a7fkBGWnopWTkWZwFOUTo_L_kg</a></td></tr>' +
+        '<tr><td>박대렬</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjY3OTU2MDc3LCJleHAiOjMzMjI1NTU2MDc3fQ.bvIBhieQ4p0cjRz9Yv0Y-fgfOst9bGvSP9ozzkaUAjA</a></td></tr>' +
+        '<tr><td>박인영</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNjY3OTU2MDkwLCJleHAiOjMzMjI1NTU2MDkwfQ.cUpFp3VOQ7uWFDPRa3VHkmkCuQRpYpGOxdEgUT4jk_Q</a></td></tr>' +
+        '<tr><td>이재욱</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjY3OTU2MTAzLCJleHAiOjMzMjI1NTU2MTAzfQ.JFn1BNt_4uSC-Z63W2xVHiukt7ZYkeXJ1FLg0llmfjQ</a></td></tr>' +
+        '<tr><td>현지현_6</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiaWF0IjoxNjY3OTU2MTM4LCJleHAiOjMzMjI1NTU2MTM4fQ.6sqMKewK_jQOSorNfNZR8qQUHIBb_dat3P4v-6inMgE</a></td></tr>' +
+        '<tr><td>현지현_7</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjY3OTU2MTYxLCJleHAiOjMzMjI1NTU2MTYxfQ.GIU54JYVdoTgcMhD9cG98W387MlXe142H3KO7mTBpk8</a></td></tr>' +
+        '<tr><td>현지현_8</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNjY3OTU2MTcyLCJleHAiOjMzMjI1NTU2MTcyfQ.EijITOOKYNREmumrS-vwVKxEsLTnvlQWc0q4VsXAcz8</a></td></tr>' +
+        '<tr><td>현지현_9</td><td><a>eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTY2Nzk1NjE4MSwiZXhwIjozMzIyNTU1NjE4MX0.FMOJKJZSsqtxjrVfnFZSseLyBcLA3MlcYl9RU7tUCMA</a></td></tr>' +
+        '</table>' +
+        '</div>' +
+        '</div>',
     )
     .setVersion('1.0.0')
-    .addServer(isDev ? `http://${localIp}:${port}` : `https://${isCloudDev ? 'dev.' : ''}team-buildup.shop`)
+    // .addServer(isDev ? `http://${localIp}:${port}` : `https://${isCloudDev ? 'dev.' : ''}team-buildup.shop`)
+    .addServer(isDev ? `http://192.168.0.115:4000` : `https://${isCloudDev ? 'dev.' : ''}team-buildup.shop`)
+    .addServer('http://14.47.254.38:4000')
+    .addServer('http://116.255.117.119:4000')
+    .addServer('http://192.168.35.135:4000')
+    .addServer('http://192.168.0.14:4000')
     // common
     .addTag('auth', '권한 REST APIs')
     .addTag('files', '파일 업로드 REST APIs')
