69d29586ba62f074c35ae2f1543fd323f3d6e9c0
PIYoung <dlsdudg15@naver.com>
2022-12-14 14:35:53 +0900

    Create app.test.ts

---
+++ src/test/app.test.ts
@@ -0,0 +1,21 @@
+// import { CatsController } from './cats.controller';
+// import { CatsService } from './cats.service';
+
+// describe('CatsController', () => {
+//   let catsController: CatsController;
+//   let catsService: CatsService;
+
+//   beforeEach(() => {
+//     catsService = new CatsService();
+//     catsController = new CatsController(catsService);
+//   });
+
+//   describe('findAll', () => {
+//     it('should return an array of cats', async () => {
+//       const result = ['test'];
+//       jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+
+//       expect(await catsController.findAll()).toBe(result);
+//     });
+//   });
+// });
